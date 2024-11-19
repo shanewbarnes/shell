fe21eaf027bc62bedfa0d9e3d1019586855c4744
Shane Barnes <shanewbarnes@outlook.com>
2024-11-18 20:01:05 -0500

    Implemented basic loop

---
+++ shell.c
@@ -0,0 +1,24 @@
+
+int main(int argc, char **argv)
+{
+	sh_loop();
+
+	return EXIT_SUCCESS;
+}
+
+void sh_loop(void)
+{
+	char *line;
+	char **args;
+	int status;
+
+	do {
+		printf("$ ");
+		line = sh_read_line();
+		args = sh_split_line(line);
+		status = sh_execute(args);
+
+		free(line);
+		free(args);
+	} while (status);
+}
